98e7826b8dc2ac67749579cb08b867643e97d924
Pg Gonni <59372630+sekeidesign@users.noreply.github.com>
2025-06-27 07:54:13 -0400

    Add additional info

--- .next/static/chunks/app_page_tsx_b2f3bef0._.js
+++ .next/static/chunks/app_page_tsx_b2f3bef0._.js
@@ -1,8 +1,11 @@
 (globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
     script: typeof document === "object" ? document.currentScript : undefined,
     chunks: [
-  "static/chunks/node_modules_50c1e845._.js",
-  "static/chunks/app_ui-experiments_family-status-button_tsx_7fee2eca._.js"
+  "static/chunks/node_modules_next_6bab6250._.js",
+  "static/chunks/node_modules_framer-motion_dist_es_5e916064._.js",
+  "static/chunks/node_modules_motion-dom_dist_es_0da7774d._.js",
+  "static/chunks/node_modules_40745070._.js",
+  "static/chunks/app_32830564._.js"
 ],
     source: "dynamic"
 });
--- app/page.tsx
+++ app/page.tsx
@@ -1,99 +1,102 @@
-import { Tooltip } from '@ark-ui/react/tooltip'
-import { CommandLineIcon } from "@heroicons/react/16/solid";
-import Link from "next/link";
+"use client";
+
 import FamilyStatusButton from "./ui-experiments/family-status-button";
+import { Experiment } from "./ui-kit/Experiment";
+import { TextLink } from "./ui-kit/TextLink";
 
 export default function Home() {
 	return (
 		<div className="font-[family-name:var(--font-geist-sans)]">
-      
-
-      
-			<main className="flex flex-col gap-8 p-8 mt-8 items-center justify-center container mx-auto max-w-screen-md">
-				<div className="flex flex-col gap-4 w-full">
-					<h2 className="text-xl text-gray-800 font-[500] leading-none">
-						Transaction status button
+			<main className="flex flex-col gap-8 p-4 md:p-8 mt-8 items-center justify-center container mx-auto max-w-screen-md">
+				<div className="w-full pt-20">
+					<h1 className="text-sm font-[500] text-gray-800">PG Gonni</h1>
+					<h2 className="text-sm font-[500] text-gray-500">
+						Design Engineer based in Montréal, QC
 					</h2>
-					<div className="flex items-center gap-2">
-						<div className="text-xs font-[450] font-mono text-gray-500 bg-gray-200/60 p-1 px-2 w-fit rounded-lg">
-							motion
-						</div>
-						<div className="text-xs font-[450] font-mono text-gray-500 bg-gray-200/60 p-1 px-2 w-fit rounded-lg">
-							tailwindcss
-						</div>
-						<div className="text-xs font-[450] font-mono text-gray-500 bg-gray-200/60 p-1 px-2 w-fit rounded-lg">
-							nextjs
-						</div>
-					</div>
-					<div className="relative shadow-skew size-24 flex items-center justify-center rounded-xl border border-gray-200 w-full p-10 min-h-[320px] h-fit bg-white">
-						<Tooltip.Root positioning={{ placement: "top" }} openDelay={0} closeDelay={0}>
-              <Tooltip.Trigger asChild>
-                <Link
-                  href="https://github.com/sekeidesign/ui-components/blob/main/app/ui-experiments/family-status-button.tsx"
-                  target="_blank"
-                  className="absolute top-2 right-2 bg-gray-200/60 hover:bg-gray-200 hover:text-gray-700 size-7 flex items-center justify-center rounded-md text-gray-500"
-                  >
-                  <CommandLineIcon className="w-4 h-4" />
-                </Link>
-              </Tooltip.Trigger>
-              <Tooltip.Positioner>
-      <Tooltip.Content className='bg-gray-900 text-gray-50 font-[450] p-2 py-1 text-xs rounded-md'>View source code</Tooltip.Content>
-    </Tooltip.Positioner>
-            </Tooltip.Root>
-            <div className="scale-90 md:scale-100">
 
-						<FamilyStatusButton />
-            </div>
-					</div>
-					<div className="space-y-2">
-						<h3 className="text-sm text-gray-600 font-[500] leading-none">
-							Learnings
-						</h3>
-						<p className="text-sm text-gray-500 font-[420] leading-relaxed space-y-1">
-							
-								In my first attempt the text had a &quot;jelly&quot;
-								effect, particularly when animating between the &quot;Analyzing&quot; and
-								&quot;Success&quot; states. <br />
-							
-								Thankfully I had recently read about{" "}
-								<TextLink
-									href="https://www.nan.fyi/magic-motion"
-									target="_blank"
-								>
-									how Motion&apos;s &quot;magic&quot; layout property works under the hood
-								</TextLink>
-								, which made it clear that this was caused by the text trying to
-								transform from one string to the next. <br />
-							
-								The fix was simply to add a unique key to the text element for
-								each state, which ensures only the button magically transforms
-								between states, while the label simply translates on the x axis and fades in and out.
-							
+					<div className="text-sm text-gray-500 font-[450] pt-10 leading-relaxed space-y-2">
+						<p>
+							At the moment, I'm Head of Design at{" "}
+							<TextLink
+								href="https://www.planned.com"
+								target="_blank"
+								hasFavicon
+							>
+								Planned
+							</TextLink>
+							. <br />
+							Before that, I was a Design Engineer at{" "}
+							<TextLink href="https://www.metafy.gg" target="_blank" hasFavicon>
+								Metafy
+							</TextLink>{" "}
+							and a Product Designer at{" "}
+							<TextLink href="https://metalab.co" target="_blank" hasFavicon>
+								Metalab
+							</TextLink>
+							.
 						</p>
 					</div>
+					<p className="text-sm text-gray-500 font-[450] pt-10 leading-relaxed">
+						Below are some UI experiments I've been cooking up to practice
+						micro-interactions and animations.
+					</p>
 				</div>
+
+				<hr className="w-full border-gray-200 my-20" />
+
+				<Experiment sourceUrl="https://github.com/sekeidesign/ui-components/blob/main/app/ui-experiments/family-status-button.tsx">
+					<Experiment.Title>Transaction status button</Experiment.Title>
+					<Experiment.Tags>
+						<Experiment.Tag>motion</Experiment.Tag>
+						<Experiment.Tag>tailwind</Experiment.Tag>
+						<Experiment.Tag>react</Experiment.Tag>
+					</Experiment.Tags>
+					<Experiment.Example>
+						<FamilyStatusButton />
+					</Experiment.Example>
+					<Experiment.Description>
+						In my first attempt the text had a &quot;jelly&quot; effect,
+						particularly when animating between the &quot;Analyzing&quot; and
+						&quot;Success&quot; states. <br />
+						Thankfully I had recently read about{" "}
+						<TextLink
+							href="https://www.nan.fyi/magic-motion"
+							target="_blank"
+							hasFavicon
+						>
+							how Motion&apos;s &quot;magic&quot; layout property works
+						</TextLink>
+						, which made it clear that this was caused by the text trying to
+						transform from one string to the next. <br />
+						The fix was simply to add a unique key to the text for each state,
+						which ensures only the button magically transforms between states,
+						while the label simply translates on the x axis and fades in and
+						out.
+					</Experiment.Description>
+				</Experiment>
+				<hr className="w-full border-gray-200 my-20" />
 			</main>
-      
+			<footer className="flex gap-8 items-center justify-between container mx-auto max-w-screen-md text-sm px-4 md:px-8 pb-20">
+				<div className="flex gap-4">
+					<TextLink
+						href="https://github.com/sekeidesign/"
+						target="_blank"
+						hasFavicon
+					>
+						GitHub
+					</TextLink>
+					<TextLink
+						href="https://www.threads.com/@sekeidesign"
+						target="_blank"
+						hasFavicon
+					>
+						Threads
+					</TextLink>
+				</div>
+				<p className="text-gray-400 leading-relaxed">
+					Updated on June 27, 2025
+				</p>
+			</footer>
 		</div>
 	);
 }
-
-const TextLink = ({
-	href,
-	children,
-	target,
-}: {
-	href: string;
-	children: React.ReactNode;
-	target: string;
-}) => {
-	return (
-		<Link
-			href={href}
-			className="text-blue-500 font-[500] underline hover:no-underline"
-			target={target}
-		>
-			{children}
-		</Link>
-	);
-};
