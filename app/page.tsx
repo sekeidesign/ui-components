12146545ed423115a0f746a036d06e425f6f00cd
sekeidesign <p.gonni@hotmail.com>
2025-12-26 10:26:12 -0500

    Fix fragment

--- app/page.tsx
+++ app/page.tsx
@@ -6,27 +6,28 @@ import { ProfileMenuExperiment } from "./ui-experiments/profile-menu/Experiment"
 import { TransactionStatusButtonExperiment } from "./ui-experiments/transaction-status-button/Experiment";
 import { VerticalIconSwitchExperiment } from "./ui-experiments/vertical-icon-switch/Experiment";
 import { ExperimentDivider } from "./ui-kit/Experiment";
+import { Fragment } from "react";
 
 const experiments = [
-	ProfileMenuExperiment,
-	DynamicIslandExperiment,
-	TransactionStatusButtonExperiment,
-	CollapsableMenuExperiment,
-	VerticalIconSwitchExperiment,
+  ProfileMenuExperiment,
+  DynamicIslandExperiment,
+  TransactionStatusButtonExperiment,
+  CollapsableMenuExperiment,
+  VerticalIconSwitchExperiment,
 ];
 
 export default function Home() {
-	return (
-		<>
-			{experiments.map((experiment, index) => {
-				const ExperimentComponent = experiment;
-				return (
-					<>
-						<ExperimentComponent key={experiment.name} />
-						{index < experiments.length - 1 && <ExperimentDivider />}
-					</>
-				);
-			})}
-		</>
-	);
+  return (
+    <>
+      {experiments.map((experiment, index) => {
+        const ExperimentComponent = experiment;
+        return (
+          <Fragment key={experiment.name}>
+            <ExperimentComponent />
+            {index < experiments.length - 1 && <ExperimentDivider />}
+          </Fragment>
+        );
+      })}
+    </>
+  );
 }
