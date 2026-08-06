e94c54522650527e0a93eeba6cde80e4eb3edb47
Pg Gonni <59372630+sekeidesign@users.noreply.github.com>
2026-08-06 16:53:16 -0400

    Add dividers

--- app/home-alt/page.tsx
+++ app/home-alt/page.tsx
@@ -2,7 +2,7 @@ import { AboutPanel } from "../AboutPanel";
 import { AppsPanel } from "../AppsPanel";
 import { GlobePanel } from "../GlobePanel";
 import { LibraryPanel } from "../LibraryPanel";
-import { Experiment } from "../ui-kit/Experiment";
+import { Experiment, ExperimentDivider } from "../ui-kit/Experiment";
 import { HoverProvider } from "../ui-kit/HoverContext";
 import { WipBanner } from "../WipBanner";
 import { WorkExperiencePanelVertical } from "./WorkExperiencePanelVertical";
@@ -15,7 +15,13 @@ export default function HomeAlt() {
 				<div className="flex gap-px bg-gray-200">
 					<AboutPanel />
 				</div>
+			</Experiment>
+			<ExperimentDivider />
+			<Experiment className="p-0 md:p-0 gap-px bg-gray-200! flex flex-col xl:max-w-3xl">
 				<WorkExperiencePanelVertical />
+			</Experiment>
+			<ExperimentDivider />
+			<Experiment className="p-0 md:p-0 gap-px bg-gray-200! flex flex-col xl:max-w-3xl">
 				<div className="flex gap-px bg-gray-200">
 					<GlobePanel />
 					<AppsPanel />
